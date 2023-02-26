ecd305bf776a042d3df8962bd4523e984801ed8b
Mikhail <jackglowpetux@gmail.com>
2023-02-26 09:25:05 +0500

    Initialize the project

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
 import flask
+
+app = flask.Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
